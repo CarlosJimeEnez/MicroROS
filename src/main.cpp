390cbecc2defe2c55a984315f51e67bdb43e7110
jose_carlos <jjimenezpena7@gmail.com>
2023-02-22 11:03:38 -0600

    Lectura del valor defusificado

--- src/main.cpp
+++ src/main.cpp
@@ -40,13 +40,15 @@ const char *mqtt_username = "emqx";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 
 // UROS Dependencias: 
 rcl_publisher_t publisher;
-std_msgs__msg__Int32 msg;
+std_msgs__msg__Int32 encoder_data;
+
 
 rclc_executor_t executor;
 rclc_support_t support;
@@ -55,20 +57,52 @@ rcl_node_t node;
 rcl_timer_t timer;
 
 
+// Flag Encoder Subscription: 
+rcl_subscription_t subscriber;
+std_msgs__msg__Int32 msg2;
+
+
+
 // ENCODER Configuration: 
 Encoder encoder1(0,0,0); 
 
+
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
+
+
+// ---------- ROS_CALLBACK ENCODER:  ---------------- 
+void subscription_callback(const void * msgin)
+{
+  // Cast received message to used type
+  const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin; 
+  // Process message
+  RCSOFTCHECK(rcl_publish(&publisher, &encoder_data, NULL));
+
+  encoder1.mapVal(); 
+  encoder_data.data = encoder1.SumDegTotal(360); 
+
+  int msg_int = msg->data;   
+  if(0 < msg_int && msg_int < 5){
+    client.publish("esp32/test/rsautomation", "0");
+  }else{
+    client.publish("esp32/test/rsautomation", "1");
+  }
+    
+  RCSOFTCHECK(rcl_publish(&publisher, &encoder_data, NULL));
+}
+
 
 // Error handle loop
 void error_loop() {
   while(1) {
     delay(100);
+    client.publish("esp32/test/rsautomation", "ERROR"); 
   }
 }
 
 
+// Callback MQTT: 
 void callback(char *topic, byte *payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
@@ -81,30 +115,17 @@ void callback(char *topic, byte *payload, unsigned int length) {
 }
 
 
-void subscription_callback(const void * msgin)
-{
-  client.publish(topic, "callback"); 
-}
-
-
-void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
-  RCLC_UNUSED(last_call_time);
-  if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg, NULL));
-  }
-}
-
-
 /// --------------------------  SETUP ---------------------------------- ///
 void setup() {
+  pinMode(GPIO_NUM_0, OUTPUT); 
 
   // ---------------- Serial port Configuration: ---------------- //
   Serial.begin(115200);
 
-  // ----------------- I2c Configuration: ----------------- // 
+  // ------------------ I2c Configuration: ------------------ // 
   Wire.begin(); 
 
-  // connecting to a WiFi network
+  // --------------------- Connecting to a WiFi network -------------------------- //
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       delay(500);
@@ -131,7 +152,7 @@ void setup() {
   client.subscribe(topic);
   
 
-  // --------------------- ROS Configuration: ------------------- // 
+  // ----------------------- ROS Configuration: --------------------- // 
   set_microros_serial_transports(Serial);
   delay(2000);
 
@@ -142,54 +163,37 @@ void setup() {
 
   // create node
   RCCHECK(rclc_node_init_default(&node, "micro_ros_platformio_node", "", &support));
-
-  // create publisher
+  // ---------------------------- Published topics ---------------------------- :
   RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
     "micro_ros_platformio_node_publisher"));
 
-  // // create publisher
-  // RCCHECK(rclc_publisher_init_default(
-  //   &publisher,
-  //   &node,
-  //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-  //   "debug_function"));
-
-
-  // //create subscriber
-  // RCCHECK(rclc_subscription_init_default(
-  //   &subscriber,
-  //   &node, 
-  //   ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-  //   "defuzzified_value"
-  // ));
-
-
-  // create timer,
-  const unsigned int timer_timeout = 10;
-  RCCHECK(rclc_timer_init_default(
-    &timer,
-    &support,
-    RCL_MS_TO_NS(timer_timeout),
-    timer_callback));
   
-  msg.data = 0;
+  //// ---------------------------- Subscribed topics ---------------------------- :   
+  RCCHECK(rclc_subscription_init_default(
+    &subscriber,
+    &node, 
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "flag_encoder"
+  ));
 
+
+  // Mensajes: 
+  encoder_data.data = 0;
+  msg2.data = 0;
+  
   // create executor
   RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
-  //RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg2, &subscription_callback, ON_NEW_DATA));   
-  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+  RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg2, &subscription_callback, ON_NEW_DATA));
+ 
   // --------- Setup Encoder -------- //
-    encoder1.setupCero(); 
+  encoder1.setupCero(); 
 }
 
+
+// ----------------------------------------------  LOOP  ------------------------------------------- //
 void loop() {
   RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
-  
-  encoder1.mapVal(); 
-  msg.data = encoder1.SumDegTotal(360); 
-
-  client.publish(topic, "hello");
 }
